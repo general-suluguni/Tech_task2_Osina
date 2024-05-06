6321e2c4fbd34a8e79ea9addfe09f02c7987c9b4
general-suluguni <167622542+general-suluguni@users.noreply.github.com>
2024-05-06 21:41:46 +0300

    Created test_generator.java
    Эта программа позволяет генерировать тестовые файлы любого необходимого размера

---
+++ test_generator.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Long> numbers = new ArrayList<>();
+        Random random = new Random();
+        System.out.println("Введите необходимое количество чисел в файле: ");
+        Scanner scan = new Scanner(System.in);
+        try {
+            int len = scan.nextInt();
+            for (int i = 0; i < len; i++) {
+                numbers.add(random.nextLong());
+            }
+            String testfile_name = "generated_test.txt";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(testfile_name))) {
+                for (Long num : numbers) {
+                    writer.write(num.toString());
+                    writer.write(" ");
+                }
+            } catch (IOException ex) {
+                System.err.println("Ошибка при записи файла: " + ex.getMessage());
+            }
+            System.out.println("Тестовый файл успешно создан!");
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("Введено некорректное число: " + ex.getMessage());
+        }
+    }
+}
